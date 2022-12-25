c637534a1f6ed05c2456528ad2c49ff74eaec50a
omanar <omanar@student.1337.ma>
2022-12-25 14:13:32 +0100

    cpp01/ex02

---
+++ C++ Module 01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omanar <omanar@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/25 13:56:49 by omanar            #+#    #+#             */
+/*   Updated: 2022/12/25 14:12:46 by omanar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main() {
+	std::string		string("HI THIS IS BRAIN");
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "Memory addresses" << std::endl;
+	std::cout << "  | string    : " << &string << std::endl;
+	std::cout << "  | stringPTR : " << stringPTR << std::endl;
+	std::cout << "  | stringREF : " << &stringREF << std::endl;
+
+	std::cout << "\nValues" << std::endl;
+	std::cout << "  | string    : " << string << std::endl;
+	std::cout << "  | stringPTR : " << *stringPTR << std::endl;
+	std::cout << "  | stringREF : " << stringREF << std::endl;
+	return 0;
+}
