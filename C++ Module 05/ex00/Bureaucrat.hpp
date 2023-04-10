0937ad721b17586cfc6e1d3bb60ac791c3a99267
omanar <omanar@student.1337.ma>
2023-04-10 07:53:37 +0000

    cpp05/ex00

---
+++ C++ Module 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omanar <omanar@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 07:35:54 by omanar            #+#    #+#             */
+/*   Updated: 2023/04/10 07:46:18 by omanar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		std::string const	name;
+		int					grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string const &name, int grade);
+		Bureaucrat(Bureaucrat const &src);
+		~Bureaucrat();
+		Bureaucrat &operator=(Bureaucrat const &rhs);
+
+		std::string const	getName() const;
+		int					getGrade() const;
+		void				incrementGrade();
+		void				decrementGrade();
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &obj);
---
+++ C++ Module 05/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omanar <omanar@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 07:46:07 by omanar            #+#    #+#             */
+/*   Updated: 2023/04/10 07:50:52 by omanar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main() {
+	try
+	{
+		Bureaucrat Steven("Steven", 1);
+		Steven.incrementGrade();
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
