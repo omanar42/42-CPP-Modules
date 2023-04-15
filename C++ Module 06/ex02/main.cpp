6a678d3ff365eb134f1765efde7bc4e942a4e5d0
omanar <omanar@student.1337.ma>
2023-04-15 22:23:40 +0000

    cpp06/ex02

---
+++ C++ Module 06/ex02/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omanar <omanar@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 21:50:39 by omanar            #+#    #+#             */
+/*   Updated: 2023/04/15 22:22:01 by omanar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void) {
+	int i = time(NULL) % 3;
+
+	if (i == 0)
+		return new A();
+	else if (i == 1)
+		return new B();
+	else
+		return new C();
+}
+
+void identify(Base* p) {
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p) {
+	try {
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+		return;
+	} catch (std::bad_cast&) {}
+
+	try {
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+		return;
+	} catch (std::bad_cast&) {}
+
+	try {
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+		return;
+	} catch (std::bad_cast&) {}
+}
+
+int main() {
+	Base *obj = generate();
+
+	identify(obj);
+	identify(*obj);
+
+	delete obj;
+
+	return 0;
+}
