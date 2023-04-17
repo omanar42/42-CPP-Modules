e43186b9b6a70e50c60efdbfb279050df81d6c63
omanar <omanar@student.1337.ma>
2023-04-17 01:45:41 +0000

    edit cpp07/ex01

--- C++ Module 07/ex01/main.cpp
+++ C++ Module 07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omanar <omanar@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 00:11:09 by omanar            #+#    #+#             */
-/*   Updated: 2023/04/17 00:22:56 by omanar           ###   ########.fr       */
+/*   Updated: 2023/04/17 01:45:01 by omanar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 template <typename T>
 void display(T &i) {
-	std::cout << i << std::endl;
+	std::cout << "   | " << i << std::endl;
 }
 
 int main() {
-	std::cout << "   | Test with int array |" << std::endl;
+	std::cout << "Test with int array" << std::endl;
 	int arr[] = {1, 2, 3, 4, 5};
 	iter(arr, 5, display);
 
-	std::cout << "   | Test with char array |" << std::endl;
+	std::cout << "\nTest with char array" << std::endl;
 	char arr2[] = {'a', 'b', 'c', 'd', 'e'};
 	iter(arr2, 5, display);
 
