0837b2f6c35074c5ba87989076bb94d4b9d72fb6
omanar <omanar@student.1337.ma>
2023-04-17 00:23:19 +0000

    cpp07/ex01

---
+++ C++ Module 07/ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omanar <omanar@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 00:11:09 by omanar            #+#    #+#             */
+/*   Updated: 2023/04/17 00:22:56 by omanar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+template <typename T>
+void display(T &i) {
+	std::cout << i << std::endl;
+}
+
+int main() {
+	std::cout << "   | Test with int array |" << std::endl;
+	int arr[] = {1, 2, 3, 4, 5};
+	iter(arr, 5, display);
+
+	std::cout << "   | Test with char array |" << std::endl;
+	char arr2[] = {'a', 'b', 'c', 'd', 'e'};
+	iter(arr2, 5, display);
+
+	return 0;
+}
