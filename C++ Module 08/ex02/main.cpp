64d9a03660fb434dd40d669e3bdc8ec54115c0b1
omanar <omanar@student.1337.ma>
2023-05-16 11:54:59 +0100

    add more tests

--- C++ Module 08/ex02/main.cpp
+++ C++ Module 08/ex02/main.cpp
@@ -6,29 +6,28 @@
 /*   By: omanar <omanar@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 17:04:47 by omanar            #+#    #+#             */
-/*   Updated: 2023/05/15 09:27:32 by omanar           ###   ########.fr       */
+/*   Updated: 2023/05/16 11:54:00 by omanar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main()
+int	main()
 {
-	MutantStack<int>	mstack;
+	MutantStack<int> mstack;
 
 	mstack.push(5);
 	mstack.push(17);
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << "   | mstack top: " << mstack.top() << std::endl;
 
 	mstack.pop();
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << "   | mstack size: " << mstack.size() << std::endl;
 
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
@@ -36,11 +35,29 @@ int main()
 
 	++it;
 	--it;
+	std::cout << "   | mstack: ";
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *it << " ";
 		++it;
 	}
+	std::cout << std::endl << std::endl;
 	std::stack<int> s(mstack);
+
+	std::cout << "   | s size: " << s.size() << std::endl;
+	std::cout << "   | s top: " << s.top() << std::endl;
+	std::cout << "   | s: ";
+	while (!s.empty())
+	{
+		std::cout << s.top() << " ";
+		s.pop();
+	}
+	std::cout << std::endl;
+
+	if (s.empty())
+		std::cout << "   | s now is empty" << std::endl;
+	else
+		std::cout << "   | s still not empty" << std::endl;
+
 	return 0;
 }
